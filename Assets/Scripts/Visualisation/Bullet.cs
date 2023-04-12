ec4f1992f5857d8318dbec6f397a14757c70a8bd
kanitaa <janita.kor@gmail.com>
2023-04-12 14:57:59 +0300

    element png added

--- Assets/Scripts/Visualisation/Bullet.cs
+++ Assets/Scripts/Visualisation/Bullet.cs
@@ -1,17 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class Bullet : MonoBehaviour
 {
     public Element element;
+    PhotonView pv;
+    private void Awake()
+    {
+        pv = GetComponent<PhotonView>();
+        if(pv.IsMine)
+            Invoke("DestroySpell",5);
+    }
     private void OnCollisionEnter(Collision collision)
     {
+        if (!pv.IsMine) return;
         if (collision.gameObject.CompareTag("Enemy"))
         {
             collision.gameObject.GetComponent<EnemyHealth>().TakeDamage(25);
         }
-        Destroy(this.gameObject);
+        DestroySpell();
     }
 
+    void DestroySpell()
+    {
+        pv.RPC("RPC_DestroySpell", RpcTarget.All);
+    }
+    [PunRPC]
+    void RPC_DestroySpell()
+    {
+        Destroy(gameObject);
+    }
 }
