82728e4c840f7dfd6a5a61bfccb362eccbcc11b5
villev00 <ville.varhala@gmail.com>
2023-04-15 22:13:56 +0300

    Both door open now

--- Assets/Scripts/Logic/Puzzle/PuzzleManager.cs
+++ Assets/Scripts/Logic/Puzzle/PuzzleManager.cs
@@ -4,12 +4,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Logic.Enemy;
 using UnityEngine;
+using Photon.Pun;
 
 public class PuzzleManager : MonoBehaviour
 {
    // create singleton
     public static PuzzleManager instance;
-   public PuzzleData pData = new PuzzleData();
+    public PuzzleData pData = new PuzzleData();
 
     public GameObject player;
     //public event System.Action OnPuzzle3Solved;
@@ -25,9 +26,9 @@ public class PuzzleManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+   
     public void CheckPuzzle1()
-    {
-        
+    {      
         if (pData.isSolved1)
         {
             Debug.Log("Puzzle 1 solved");
@@ -57,6 +58,7 @@ public class PuzzleManager : MonoBehaviour
         }
     }
 
+  
     public void CheckAllPuzzles()
     {
        
@@ -67,10 +69,14 @@ public class PuzzleManager : MonoBehaviour
 
             if (pData.hasOtherPlayerSolvedPuzzles)
             {
-                Debug.Log("Other player solved all");
-                GameObject.FindGameObjectWithTag("Fence").GetComponent<FenceClose>().OpenFence();
-                GameObject.FindGameObjectWithTag("Fence2").GetComponent<FenceClose>().OpenFence();
+                Debug.Log("Other player solved all");               
             }               
         }       
-    }   
+    }
+    
+    public void OpenFences()
+    {                                
+                GameObject.FindGameObjectWithTag("Fence").GetComponent<FenceClose>().OpenFence();
+                GameObject.FindGameObjectWithTag("Fence2").GetComponent<FenceClose>().OpenFence();                    
+    }
 }
