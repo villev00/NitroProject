119e417929e04ea3ead719f7848eb4ef40d45aaf
kanitaa <janita.kor@gmail.com>
2023-04-16 00:08:11 +0300

    Revert "asd"
    This reverts commit b28a9686a970c36f3619f73bd1a09c2a252be0eb, reversing
    changes made to 2d42b17de5d328b5fa2731df781d90b7fd3fead7.

--- Assets/Scripts/SpellScripts/SpellUI.cs
+++ Assets/Scripts/SpellScripts/SpellUI.cs
@@ -11,7 +11,6 @@ using UnityEngine.UI;
 
 public class SpellUI : MonoBehaviour
 {
-    [SerializeField] GameObject[] spellSlots;
     public GameObject[] fireSlots;
     public GameObject[] aetherSlots;
     public GameObject[] lightningSlots;
@@ -24,81 +23,61 @@ public class SpellUI : MonoBehaviour
     Spell[] currentSpells= new Spell[3];
     private void Start()
     {
-       // activeSlots = fireSlots;
+        activeSlots = fireSlots;
     }
     private void Update()
     {
         if (!pv.IsMine) return;
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            spellSlots[0].GetComponent<Button>().onClick.Invoke();
-           // activeSlots[0].GetComponent<Button>().onClick.Invoke();
+            activeSlots[0].GetComponent<Button>().onClick.Invoke();
              ShowSpellCooldown(currentSpells[0], 0);
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            spellSlots[1].GetComponent<Button>().onClick.Invoke();
-           // activeSlots[1].GetComponent<Button>().onClick.Invoke();
+            activeSlots[1].GetComponent<Button>().onClick.Invoke();
             ShowSpellCooldown(currentSpells[1],1);
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            spellSlots[2].GetComponent<Button>().onClick.Invoke();
-         //   activeSlots[2].GetComponent<Button>().onClick.Invoke();
+            activeSlots[2].GetComponent<Button>().onClick.Invoke();
             ShowSpellCooldown(currentSpells[2],2);
         }
     }
+
+    //jos samassa spellslotissa on kaksi spelliä cd, niin timer visuaali kusee
     public void ChangeSpellSet(Spell[] spells)
     {
-      
         if (!pv.IsMine) return;
+        for (int i = 0; i < activeSlots.Length;i++)
+        {
+            if (activeSlots[i].gameObject!=null)
+            activeSlots[i].gameObject.SetActive(false);
+           
+        }
         StopAllCoroutines();
+        if (spells[0].spellElement==Element.Fire)activeSlots = fireSlots;
+        else if (spells[0].spellElement == Element.Aether) activeSlots = aetherSlots;
+        else if (spells[0].spellElement == Element.Lightning) activeSlots = lightningSlots;
+
         for (int i = 0; i < spells.Length; i++)
         {
-
-            spellSlots[i].GetComponent<Image>().sprite = spells[i].spellSprite;
-            spellSlots[i].GetComponent<Button>().onClick.RemoveAllListeners();
+            activeSlots[i].gameObject.SetActive(true);
+            activeSlots[i].GetComponent<Button>().onClick.RemoveAllListeners();
             int index = i;
-            spellSlots[index].GetComponent<Button>().onClick.AddListener(delegate { spellManager.GetComponent<Spells>().UseSpell(spells[index]); });
+            activeSlots[i].GetComponent<Button>().onClick.AddListener(delegate { spellManager.GetComponent<Spells>().UseSpell(spells[index]); });
             currentSpells[i] = spells[i];
             ShowSpellCooldown(currentSpells[i], i);
-
+           
         }
     }
-    //jos samassa spellslotissa on kaksi spelliä cd, niin timer visuaali kusee
-    //public void ChangeSpellSet(Spell[] spells)
-    //{
-    //    if (!pv.IsMine) return;
-    //    for (int i = 0; i < activeSlots.Length;i++)
-    //    {
-    //        if (activeSlots[i].gameObject!=null)
-    //        activeSlots[i].gameObject.SetActive(false);
-           
-    //    }
-    //    StopAllCoroutines();
-    //    if (spells[0].spellElement==Element.Fire)activeSlots = fireSlots;
-    //    else if (spells[0].spellElement == Element.Aether) activeSlots = aetherSlots;
-    //    else if (spells[0].spellElement == Element.Lightning) activeSlots = lightningSlots;
-
-    //    for (int i = 0; i < spells.Length; i++)
-    //    {
-    //        activeSlots[i].gameObject.SetActive(true);
-    //        activeSlots[i].GetComponent<Button>().onClick.RemoveAllListeners();
-    //        int index = i;
-    //        activeSlots[i].GetComponent<Button>().onClick.AddListener(delegate { spellManager.GetComponent<Spells>().UseSpell(spells[index]); });
-    //        currentSpells[i] = spells[i];
-    //        ShowSpellCooldown(currentSpells[i], i);
-           
-    //    }
-    //}
     void ShowSpellCooldown(Spell spell, int index)
     {
         // Start the coroutine for this spell if its on cd
         if (spell.isSpellOnCooldown)
             StartCoroutine(Cooldown(spell, index));
         //StartCoroutine(ShowCooldown(spell, index));
-       // else activeSlots[index].transform.GetChild(0).GetComponent<Slider>().value = 0;
-        else spellSlots[index].transform.GetChild(0).GetComponent<Slider>().value = 0;
+        else activeSlots[index].transform.GetChild(0).GetComponent<Slider>().value = 0;
 
     }
     IEnumerator Cooldown(Spell spell, int index)
@@ -106,10 +85,9 @@ public class SpellUI : MonoBehaviour
         while (spell.isSpellOnCooldown)
         {
             Debug.Log("cd remaining: " + spell.cooldownRemaining);
-          //  activeSlots[index].transform.GetChild(0).GetComponent<Slider>().value = spell.cooldownRemaining / spell.spellCooldown;
-            spellSlots[index].transform.GetChild(0).GetComponent<Slider>().value = spell.cooldownRemaining / spell.spellCooldown;
+            activeSlots[index].transform.GetChild(0).GetComponent<Slider>().value = spell.cooldownRemaining / spell.spellCooldown;
             yield return new WaitForSeconds(1f);
         }
-        spellSlots[index].transform.GetChild(0).GetComponent<Slider>().value = 0;
+        activeSlots[index].transform.GetChild(0).GetComponent<Slider>().value = 0;
     }
 }
