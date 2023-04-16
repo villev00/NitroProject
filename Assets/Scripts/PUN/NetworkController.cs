74c2f882081bb9b33c1aef2853e0d54c63c920a6
kanitaa <janita.kor@gmail.com>
2023-04-16 12:32:23 +0300

    small menu fix

--- Assets/Scripts/PUN/NetworkController.cs
+++ Assets/Scripts/PUN/NetworkController.cs
@@ -5,36 +5,22 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NetworkController : MonoBehaviourPunCallbacks
 {
+    [SerializeField] Button startGame;
     private void Start()
     {
         PhotonNetwork.Disconnect();
         PhotonNetwork.ConnectUsingSettings(); //connects to photon master server
-
-        SceneManager.activeSceneChanged += ChangedActiveScene;
     }
 
     public override void OnConnectedToMaster()
     {
         Debug.Log("Now connected to the " + PhotonNetwork.CloudRegion + " server!");
+        startGame.interactable= true;
     }
 
-    private void ChangedActiveScene(Scene current, Scene next)
-    {
-        string currentName = current.name;
-       
-            if (currentName == null)
-        {
-            // Scene1 has been removed
-            currentName = "Replaced";
-        }
-        if (currentName == "Replaced" && next.name == "Menu")
-        {
-            PhotonNetwork.Disconnect();
-            PhotonNetwork.ConnectUsingSettings(); //connects to photon master server
-        }
-        Debug.Log("Scenes: " + currentName + ", " + next.name);
-    }
+  
 }
