11f35ce7d0a50d2147af218dabbcc2ec766d953e
villev00 <ville.varhala@gmail.com>
2023-04-19 09:12:09 +0300

    Traps

---
+++ Assets/Scripts/Data/TrapData.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace data
+{
+    [Serializable]
+
+    public class TrapData : MonoBehaviour
+    {
+        public int trapDamage = 10;
+    }
+}
---
+++ Assets/Scripts/Logic/Traps.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traps : MonoBehaviour
+{
+    public Animator animator;
+
+    public int trapDamage;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>(); 
+        InvokeRepeating("WallSpikeTrap", 0f, 5f);
+        InvokeRepeating("FloorSpikeTrap", 0f, 5f);
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Player hit by trap");
+            collision.gameObject.GetComponent<PlayerLogic>().TakeDamage(trapDamage);
+        }
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<EnemyHealth>().TakeDamage(trapDamage, 0);
+        }
+    }
+
+    public void WallSpikeTrap()
+    {
+        animator.SetBool("WallSpikeTrap", true);
+    }
+    public void FloorSpikeTrap()
+    {
+        animator.SetBool("FloorSpikeTrap", true);
+    }
+}
