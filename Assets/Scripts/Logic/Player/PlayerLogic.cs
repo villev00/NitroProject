4da07f11f670e7ed85e11cebc4c9a764bed56461
villev00 <ville.varhala@gmail.com>
2023-05-03 12:08:39 +0300

    .

--- Assets/Scripts/Logic/Player/PlayerLogic.cs
+++ Assets/Scripts/Logic/Player/PlayerLogic.cs
@@ -55,6 +55,7 @@ public class PlayerLogic : MonoBehaviour
     }
     public void TakeDamage(int damage)
     {
+        return;
         if (flameBarrier != null)
             {
                 data.shield -= damage;
