0f535fa8b34d21ca1faac1498e7a77f96352d135
villev00 <ville.varhala@gmail.com>
2023-05-03 11:30:27 +0300

    .

--- Assets/Scripts/Visualisation/PlayerControl.cs
+++ Assets/Scripts/Visualisation/PlayerControl.cs
@@ -120,6 +120,7 @@ public class PlayerControl : MonoBehaviour
     {
         moveSpeed = pLogic.GetSpeed(); // movespeed is not updated correctly
         moveSpeed *= 1.5f;
+        moveSpeed = 5;
         //jumpForce = pLogic.GetJumpForce();
         jumpForce = 1.9f; // jumpForcee is not updated correctly
     }
